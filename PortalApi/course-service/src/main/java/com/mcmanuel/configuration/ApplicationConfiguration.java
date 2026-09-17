8dba4cd239af0f923db29ea8c029a0992ecf768a
unknown <mcmanuel755@gmail.com>
2026-09-17 02:04:00 +0100

    removed the rabbit config and added kafka producer config

--- PortalApi/course-service/src/main/java/com/mcmanuel/configuration/ApplicationConfiguration.java
+++ PortalApi/course-service/src/main/java/com/mcmanuel/configuration/ApplicationConfiguration.java
@@ -4,6 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "student")
 public record ApplicationConfiguration  (
-        String exchangeName
 ) {
 }
