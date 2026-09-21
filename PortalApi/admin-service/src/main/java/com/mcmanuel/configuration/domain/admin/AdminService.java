03ce739dec54df115826aef042b9ddb8b4118c64
unknown <mcmanuel755@gmail.com>
2026-09-21 11:05:48 +0100

    edited the admin service:added some emthods

---
+++ PortalApi/admin-service/src/main/java/com/mcmanuel/configuration/domain/admin/AdminService.java
@@ -0,0 +1,7 @@
+package com.mcmanuel.configuration.domain.admin;
+
+public interface AdminService {
+
+    boolean openPortal();
+    boolean setDepartmentalCourses();
+}
---
+++ PortalApi/admin-service/src/main/java/com/mcmanuel/configuration/domain/admin/service/AdminServiceImpl.java
@@ -0,0 +1,6 @@
+package com.mcmanuel.configuration.domain.admin;
+
+import com.mcmanuel.configuration.domain.admin.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+}
