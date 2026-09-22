12975fef4a0b46f772e4ab73e2b253b872d2d08d
unknown <mcmanuel755@gmail.com>
2026-09-22 14:53:11 +0100

    changed userdetail username from email to matric number

--- PortalApi/student-service/src/main/java/com/mcmanuel/domain/student/MyUserDetails.java
+++ PortalApi/student-service/src/main/java/com/mcmanuel/domain/student/MyUserDetails.java
@@ -24,26 +24,26 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return student.getEmail();
+        return student.getMatriculationNumber();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
