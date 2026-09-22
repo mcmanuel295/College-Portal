c77d5a5761b99ea302016d941ff3028910c5a886
unknown <mcmanuel755@gmail.com>
2026-09-22 14:13:47 +0100

    all

--- PortalApi/student-service/src/main/java/com/mcmanuel/domain/student/MyUserDetails.java
+++ PortalApi/student-service/src/main/java/com/mcmanuel/domain/student/MyUserDetails.java
@@ -24,7 +24,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return student.getMatriculationNumber();
+        return student.getEmail();
     }
 
     @Override
